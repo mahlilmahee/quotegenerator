020cfa2abfb8d05423daef1522ed0fba6a7e2ea3
mahlilmahee <mahlilmahee942@gmail.com>
2023-01-29 13:32:00 +0600

    modified it as a chrome extension and so on it works finishes here

--- src/Quote.js
+++ src/Quote.js
@@ -1,30 +1,36 @@
 import React, { useState } from 'react';
-import randomcolor from 'randomcolor';
+
 import './quote.css'
 import { useQuery } from 'react-query';
 const Quote = () => {
     // const [data,setData]=useState('')
-    var color = randomcolor()
+    
     // const quoje=()=>{
     //     fetch('https://api.adviceslip.com/advice')
     //     .then(res=>res.json())
     //     .then(data=>setData(data.slip.advice))
     //    }
 
-       const { isLoading, error, data } = useQuery('repoData', () =>
+       const { isLoading, error, data , refetch } = useQuery('repoData', () =>
      fetch('https://api.adviceslip.com/advice').then(res =>
        res.json()
      )
    )
-   console.log(data?.slip?.advice,'query')
+
+   if(isLoading){
+    return <h2> Loading please wait a some moment ,As success needs patience</h2>
+   }
+   if(error){
+    return <h2>To err is human , so am I .Soon we will fix it </h2>
+   }
     return (
         <div>
-            <h3 class="heading">Wait A minute for here in Mahlil blog</h3>
-            <h5>Random quote generator</h5>
+            <h5 class="heading">Feeling distracted ?Read some positive quotes by Successfull People</h5>
+            
             <div class="quotediv">
-            <h1 style={{color:color}}>"{data?.slip?.advice}"</h1>
+            <h2 >"{data?.slip?.advice}"</h2>
             </div>
-            <button class="button"  >Click for another</button>
+            <button class="button" onClick={refetch} >Click for another</button>
         </div>
     );
 };
